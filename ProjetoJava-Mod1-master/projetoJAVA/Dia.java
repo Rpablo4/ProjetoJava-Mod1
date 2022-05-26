284280cf7255912e0a50bada30fccc48c35d822d
Ruan Pablo <222ruanpablo@gmail.com>
2022-05-26 19:46:10 -0300

    Adicionando classe Dia e suas funcionalidades

---
+++ ProjetoJava-Mod1-master/projetoJAVA/Dia.java
@@ -0,0 +1,23 @@
+package projetoJAVA;
+
+public class Dia {
+
+protected int dia;
+protected boolean concluido;
+
+public Dia(int dia){
+	this.dia=dia;
+}
+public int getDia() {
+	return dia;
+}
+public void setDia(int dia) {
+	this.dia = dia;
+}
+public boolean isConcluido() {
+	return concluido;
+}
+public void setConcluido(boolean concluido) {
+	this.concluido = concluido;
+}
+}
--- ProjetoJava-Mod1-master/projetoJAVA/TesteExercicios.java
+++ ProjetoJava-Mod1-master/projetoJAVA/TesteExercicios.java
@@ -10,6 +10,71 @@ public class TesteExercicios {
 		Scanner input = new Scanner(System.in);
 		int nivel,dia=0,op,opMenu;
 		
+		ArrayList<Dia> arrayDia = new ArrayList<>();
+		
+		Dia dia1 = new Dia(1);
+		Dia dia2 = new Dia(2);
+		Dia dia3 = new Dia(3);
+		Dia dia4 = new Dia(4);
+		Dia dia5 = new Dia(5);
+		Dia dia6 = new Dia(6);
+		Dia dia7 = new Dia(7);
+		Dia dia8 = new Dia(8);
+		Dia dia9 = new Dia(9);
+		Dia dia10 = new Dia(10);
+		Dia dia11 = new Dia(11);
+		Dia dia12 = new Dia(12);
+		Dia dia13 = new Dia(13);
+		Dia dia14 = new Dia(14);
+		Dia dia15 = new Dia(15);
+		Dia dia16 = new Dia(16);
+		Dia dia17 = new Dia(17);
+		Dia dia18 = new Dia(18);
+		Dia dia19 = new Dia(19);
+		Dia dia20 = new Dia(20);
+		Dia dia21 = new Dia(21);
+		Dia dia22 = new Dia(22);
+		Dia dia23 = new Dia(23);
+		Dia dia24 = new Dia(24);
+		Dia dia25 = new Dia(25);
+		Dia dia26 = new Dia(26);
+		Dia dia27 = new Dia(27);
+		Dia dia28 = new Dia(28);
+		Dia dia29 = new Dia(29);
+		Dia dia30 = new Dia(30);
+		
+		arrayDia.add(dia1);
+		arrayDia.add(dia2);
+		arrayDia.add(dia3);
+		arrayDia.add(dia4);
+		arrayDia.add(dia5);
+		arrayDia.add(dia6);
+		arrayDia.add(dia7);
+		arrayDia.add(dia8);
+		arrayDia.add(dia9);
+		arrayDia.add(dia10);
+		arrayDia.add(dia11);
+		arrayDia.add(dia12);
+		arrayDia.add(dia13);
+		arrayDia.add(dia14);
+		arrayDia.add(dia15);
+		arrayDia.add(dia16);
+		arrayDia.add(dia17);
+		arrayDia.add(dia18);
+		arrayDia.add(dia19);
+		arrayDia.add(dia20);
+		arrayDia.add(dia21);
+		arrayDia.add(dia22);
+		arrayDia.add(dia23);
+		arrayDia.add(dia24);
+		arrayDia.add(dia25);
+		arrayDia.add(dia26);
+		arrayDia.add(dia27);
+		arrayDia.add(dia28);
+		arrayDia.add(dia29);
+		arrayDia.add(dia30);
+		
+		
 		ArrayList<exercicios> listaExercicios = new ArrayList<>();
 		
 		// criando exercícios exercicio(nome, reps, series, comoFazer, tempoemsec)
@@ -102,98 +167,113 @@ public class TesteExercicios {
 		case 1: //Ganhar massa magra/muscular
 			
 			
-			for(dia=0;dia<=30;dia++) {
-				System.out.println("\n\t\tDIA-"+dia+"\n\tFaça os exercícios a seguir:");
-				for(int i=0; i<listaExercicios.size(); i++) {
-					if(listaExercicios.get(i).getGanharMassa()==true) {
-						listaExercicios.get(i).imprimirInfo();
-					}
-				}
-				System.out.println("\nDigite 1 se você concluiu os exercicios de hoje \nou Digite 0 para sair:");
-				op=input.nextInt();
-				if(op==1)
-					System.out.println("\n\nParabéns, você concluiu o dia "+dia);
-				else
-					break;
-				if((dia%2)==0) {
-					for(int i=0; i<listaExercicios.size(); i++) {
-						if(listaExercicios.get(i).getNomeDoExercicio()!="Ponte") {
-							listaExercicios.get(i).aumentarRepsSeries(3, 0);
-							if(listaExercicios.get(i).getReps()>30) {
-								listaExercicios.get(i).setReps(15);
-								listaExercicios.get(i).aumentarRepsSeries(0, 1);
-							}
-						}
-						else
-							listaExercicios.get(i).aumentarTempo(15);
-					}
-				}
+			for(int c=0; c<arrayDia.size();c++) {
+				if(arrayDia.get(c).isConcluido()) {
 					
+				}
+				else {
+					System.out.println("\n\t\tDIA-"+dia+"\n\tFaça os exercícios a seguir:");
+					for(int i=0; i<listaExercicios.size(); i++) {
+						if(listaExercicios.get(i).getGanharMassa()==true) {
+							listaExercicios.get(i).imprimirInfo();
+						}
+					}
+					System.out.println("\nDigite 1 se você concluiu os exercicios de hoje \nou Digite 0 para sair:");
+					op=input.nextInt();
+					if(op==1)
+						System.out.println("\n\nParabéns, você concluiu o dia "+dia);
+					else
+						break;
+					if((dia%2)==0) {
+						for(int i=0; i<listaExercicios.size(); i++) {
+							if(listaExercicios.get(i).getNomeDoExercicio()!="Ponte") {
+								listaExercicios.get(i).aumentarRepsSeries(3, 0);
+								if(listaExercicios.get(i).getReps()>30) {
+									listaExercicios.get(i).setReps(15);
+									listaExercicios.get(i).aumentarRepsSeries(0, 1);
+									arrayDia.get(c).setConcluido(true);
+								}
+							}
+							else
+								listaExercicios.get(i).aumentarTempo(15);
+								arrayDia.get(c).setConcluido(true);
+						}
+					}
+						
+				}
 			}
 			break;
 			
 		case 2: //Perder peso
-			for(dia=0;dia<=30;dia++) {
-				System.out.println("\n\t\tDIA-"+dia+"\n\tFaça os exercícios a seguir:");
-				for(int i=0; i<listaExercicios.size(); i++) {
-					if(listaExercicios.get(i).getQueimarGordura()==true) {
-						listaExercicios.get(i).imprimirInfo();
-					}
-				}
-				System.out.println("\nDigite 1 se você concluiu os exercicios de hoje \nou Digite 0 para sair:");
-				op=input.nextInt();
-				if(op==1)
-					System.out.println("\n\nParabéns, você concluiu o dia "+dia);
-				else
-					break;
-				if((dia%2)==0) {
-					for(int i=0; i<listaExercicios.size(); i++) {
-						if(listaExercicios.get(i).getNomeDoExercicio()!="Ponte") {
-							listaExercicios.get(i).aumentarRepsSeries(3, 0);
-							if(listaExercicios.get(i).getReps()>30) {
-								listaExercicios.get(i).setReps(15);
-								listaExercicios.get(i).aumentarRepsSeries(0, 1);
-							}
-						}
-						else
-							listaExercicios.get(i).aumentarTempo(15);
-					}
-				}
+			for(int c=0; c<arrayDia.size();c++) {
+				if(arrayDia.get(c).isConcluido()) {
 					
+				}
+				else {
+					System.out.println("\n\t\tDIA-"+dia+"\n\tFaça os exercícios a seguir:");
+					for(int i=0; i<listaExercicios.size(); i++) {
+						if(listaExercicios.get(i).getQueimarGordura()==true) {
+							listaExercicios.get(i).imprimirInfo();
+						}
+					}
+					System.out.println("\nDigite 1 se você concluiu os exercicios de hoje \nou Digite 0 para sair:");
+					op=input.nextInt();
+					if(op==1)
+						System.out.println("\n\nParabéns, você concluiu o dia "+dia);
+					else
+						break;
+					if((dia%2)==0) {
+						for(int i=0; i<listaExercicios.size(); i++) {
+							if(listaExercicios.get(i).getNomeDoExercicio()!="Ponte") {
+								listaExercicios.get(i).aumentarRepsSeries(3, 0);
+								if(listaExercicios.get(i).getReps()>30) {
+									listaExercicios.get(i).setReps(15);
+									listaExercicios.get(i).aumentarRepsSeries(0, 1);
+								}
+							}
+							else
+								listaExercicios.get(i).aumentarTempo(15);
+						}
+					}
+				}
 			}
 			break;
 		case 3:
-			for(dia=0;dia<=30;dia++) {
-				System.out.println("\n\t\tDIA-"+dia+"\n\tFaça os exercícios a seguir:");
-				for(int i=0; i<listaExercicios.size(); i++) {
-					if(listaExercicios.get(i).getDefinirMusculatura()==true) {
-						listaExercicios.get(i).imprimirInfo();
-						if(listaExercicios.get(i).getReps()>30) {
-							listaExercicios.get(i).setReps(15);
-							listaExercicios.get(i).aumentarRepsSeries(0, 1);;
-						}
-					}
+			for(int c=0; c<arrayDia.size();c++) {
+				if(arrayDia.get(c).isConcluido()) {
+					
 				}
-				System.out.println("\nDigite 1 se você concluiu os exercicios de hoje \nou Digite 0 para sair:");
-				op=input.nextInt();
-				if(op==1)
-					System.out.println("\n\nParabéns, você concluiu o dia "+dia);
-				else
-					break;
-				if((dia%2)==0) {
+				else {
+					System.out.println("\n\t\tDIA-"+dia+"\n\tFaça os exercícios a seguir:");
 					for(int i=0; i<listaExercicios.size(); i++) {
-						if(listaExercicios.get(i).getNomeDoExercicio()!="Ponte") {
-							listaExercicios.get(i).aumentarRepsSeries(3, 0);
+						if(listaExercicios.get(i).getDefinirMusculatura()==true) {
+							listaExercicios.get(i).imprimirInfo();
 							if(listaExercicios.get(i).getReps()>30) {
 								listaExercicios.get(i).setReps(15);
-								listaExercicios.get(i).aumentarRepsSeries(0, 1);
+								listaExercicios.get(i).aumentarRepsSeries(0, 1);;
 							}
 						}
-						else
-							listaExercicios.get(i).aumentarTempo(15);
 					}
-				}
-					
+					System.out.println("\nDigite 1 se você concluiu os exercicios de hoje \nou Digite 0 para sair:");
+					op=input.nextInt();
+					if(op==1)
+						System.out.println("\n\nParabéns, você concluiu o dia "+dia);
+					else
+						break;
+					if((dia%2)==0) {
+						for(int i=0; i<listaExercicios.size(); i++) {
+							if(listaExercicios.get(i).getNomeDoExercicio()!="Ponte") {
+								listaExercicios.get(i).aumentarRepsSeries(3, 0);
+								if(listaExercicios.get(i).getReps()>30) {
+									listaExercicios.get(i).setReps(15);
+									listaExercicios.get(i).aumentarRepsSeries(0, 1);
+								}
+							}
+							else
+								listaExercicios.get(i).aumentarTempo(15);
+						}
+					}
+				}	
 			}
 		}
 
